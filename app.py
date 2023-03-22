0bf44a30a0c79d2e3a8c8d10dd8d20c121851220
Gaurav <gauravkesharwani@gmail.com>
2023-03-22 15:18:34 -0700

    added user registration functionality

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ logger = logging.getLogger(__name__)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True, nullable=False)
+    name = db.Column(db.String(80), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     created_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
